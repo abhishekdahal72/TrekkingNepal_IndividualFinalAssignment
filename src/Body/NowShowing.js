83caaddf8996cd72a5a654c04a1ebf4e4410f63d
abhishek dahal <57648908+abhishekdahal72@users.noreply.github.com>
2021-04-02 23:00:50 +0545

    now showing is added to home movie section

---
+++ src/Body/NowShowing.js
@@ -0,0 +1,58 @@
+import { Component, state } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import num1 from '../media/endgame.jpg';
+import num2 from '../media/doctor-strange.jpg';
+import num3 from '../media/download.jpg';
+import num4 from '../media/rrr.jpg';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import { Route } from 'react-router-dom';
+import axios from 'axios';
+
+class NowShowing extends Component {
+  state = {
+    movie: [],
+  };
+  componentDidMount() {
+    axios
+      .get('http://localhost:90/movie/fetch')
+      .then((response) => {
+        console.log(response);
+        this.setState({
+          movie: response.data.data,
+        });
+      })
+      .catch((err) => {
+        console.log(err.response);
+      });
+  }
+  render() {
+    return (
+      <div className='home-nowshowing'>
+        <Row>
+          {this.state.movie.map((movie) => {
+            return (
+              <Col md={3} sm={4} xs={12}>
+                <Card className='movie-card'>
+                  <Card.Img
+                    variant='top'
+                    src={'http://localhost:90/images/' + movie.imagepp}
+                  />
+                  <Card.Body>
+                    <Card.Title>
+                      <a href={'/nssinglemoviedata/' + movie._id}>
+                        {movie.title}
+                      </a>
+                    </Card.Title>
+                    <Button variant='primary'>Book Ticket</Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+            );
+          })}
+        </Row>
+      </div>
+    );
+  }
+}
+export default NowShowing;
