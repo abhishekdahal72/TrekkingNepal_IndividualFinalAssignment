feb20a02f72c44f45f7720a17d46917544abd945
abhishek dahal <57648908+abhishekdahal72@users.noreply.github.com>
2021-04-08 14:12:40 +0545

    ticket price data is fetched from backend

--- src/Body/TicketPage.js
+++ src/Body/TicketPage.js
@@ -1,80 +1,97 @@
-import {
-  Component
-} from 'react';
+import { Component, state } from 'react';
 import { Container, Row, Col, Table } from 'react-bootstrap';
-//import axios from 'axios';
+import axios from 'axios';
 
 class TicketPage extends Component {
-  
+  state = {
+    ticket: [],
+  };
+  componentDidMount() {
+    axios
+      .get('http://localhost:90/ticket/fetch')
+      .then((response) => {
+        console.log(response);
+        this.setState({
+          ticket: response.data.data,
+        });
+      })
+      .catch((err) => {
+        console.log(err.response);
+      });
+  }
   render() {
     return (
       <Container>
         <Row>
-          <Col className='ticket-page'>
-            <h3>Ticket Rates:</h3>
-            <div className='ticket-rate'>
-              <Table
-                striped
-                bordered
-                hover
-                variant='dark'
-                className='ticket-table'
-              >
-                <thead>
-                  <tr>
-                    <th>Weeks</th>
-                    <th>Platinum Ticket</th>
-                    <th>Gold Ticket</th>
-                    <th>Silver Ticket</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>Sunday</td>
-                    <td>550</td>
-                    <td>450</td>
-                    <td>350</td>
-                  </tr>
-                  <tr>
-                    <td>Monday</td>
-                    <td>550</td>
-                    <td>450</td>
-                    <td>350</td>
-                  </tr>
-                  <tr>
-                    <td>Tuesday (30 % dis)</td>
-                    <td>550</td>
-                    <td>450</td>
-                    <td>350</td>
-                  </tr>
-                  <tr>
-                    <td>Wednesday (30 % dis)</td>
-                    <td>550</td>
-                    <td>450</td>
-                    <td>350</td>
-                  </tr>
-                  <tr>
-                    <td>Thursday</td>
-                    <td>550</td>
-                    <td>450</td>
-                    <td>350</td>
-                  </tr>
-                  <tr>
-                    <td>Friday</td>
-                    <td>550</td>
-                    <td>450</td>
-                    <td>350</td>
-                  </tr>
-                  <tr>
-                    <td>Saturday</td>
-                    <td>550</td>
-                    <td>450</td>
-                    <td>350</td>
-                  </tr>
-                </tbody>
-              </Table>
-            </div>
-          </Col>
+          {this.state.ticket.map((ticket) => {
+            return (
+              <Col className='ticket-page'>
+                <h3>Ticket Rates:</h3>
+                <div className='ticket-rate'>
+                  <Table
+                    striped
+                    bordered
+                    hover
+                    variant='dark'
+                    className='ticket-table'
+                  >
+                    <thead>
+                      <tr>
+                        <th>Weeks</th>
+                        <th>Platinum Ticket</th>
+                        <th>Gold Ticket</th>
+                        <th>Silver Ticket</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>Sunday</td>
+                        <td>{ticket.platinum_ticket}</td>
+                        <td>{ticket.gold_ticket}</td>
+                        <td>{ticket.silver_ticket}</td>
+                      </tr>
+                      <tr>
+                        <td>Monday</td>
+                        <td>{ticket.platinum_ticket}</td>
+                        <td>{ticket.gold_ticket}</td>
+                        <td>{ticket.silver_ticket}</td>
+                      </tr>
+                      <tr>
+                        <td>Tuesday (30 % dis)</td>
+                        <td>{ticket.dis_platinum_ticket}</td>
+                        <td>{ticket.dis_gold_ticket}</td>
+                        <td>{ticket.dis_silver_ticket}</td>
+                      </tr>
+                      <tr>
+                        <td>Wednesday (30 % dis)</td>
+                        <td>{ticket.dis_platinum_ticket}</td>
+                        <td>{ticket.dis_gold_ticket}</td>
+                        <td>{ticket.dis_silver_ticket}</td>
+                      </tr>
+                      <tr>
+                        <td>Thursday</td>
+                        <td>{ticket.platinum_ticket}</td>
+                        <td>{ticket.gold_ticket}</td>
+                        <td>{ticket.silver_ticket}</td>
+                      </tr>
+                      <tr>
+                        <td>Friday</td>
+                        <td>{ticket.platinum_ticket}</td>
+                        <td>{ticket.gold_ticket}</td>
+                        <td>{ticket.silver_ticket}</td>
+                      </tr>
+                      <tr>
+                        <td>Saturday</td>
+                        <td>{ticket.platinum_ticket}</td>
+                        <td>{ticket.gold_ticket}</td>
+                        <td>{ticket.silver_ticket}</td>
+                      </tr>
+                    </tbody>
+                  </Table>
+                </div>
+              </Col>
+            );
+          })}
         </Row>
       </Container>
     );
